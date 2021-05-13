c36d63d2738dd4c7d392cdcbf0cc85f08380781f
THE11th <vonatsiv1030@gmail.com>
2021-05-13 11:43:35 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,8 +9,8 @@ client.on("ready", readyDiscord)
 
 function readyDiscord() {
     console.log("Bot is online")
-    client.user.setActivity('pumping lemmas', { type: 'WATCHING' })
-    testLoop()
+    client.user.setActivity('$bonkbot', { type: 'PLAYING' })
+    demo()
 }
 
 client.on("message", gotMessage)
@@ -25,6 +25,26 @@ const inspireArr = [
     "It's all downhill from here"
 ]
 
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo(){
+    let date
+    while(true){
+        date = new Date()
+        // console.log(date.getHours())
+        // console.log()
+        // console.log(date.getMinutes())
+        await sleep(60000)
+
+        if(date.getHours() % 3 == 0 && date.getMinutes() == 29)
+        for (const [key, value] of Object.entries(claimTimer)) {
+            claimTimer[key] = "can"
+        }
+    }
+}
+
 function randomNumber(min, max){
     const r = Math.random()*(max-min) + min
     return Math.floor(r)
@@ -35,6 +55,22 @@ var timeReset = false
 
 function gotMessage(msg){
     var message = msg.content.split(" ")
+    //console.log(message)
+
+    if(message[0] === "$bonkbot") {
+        msg.reply(`🔨 BONK BOT LIST OF COMMANDS 🔨
+        **$bonk** -> <$bonk @someone> to send them to horny jail 
+        **$trash** -> <$trash @someone> to tell them that they're trash 
+        **$inspire** -> receive a random "motivational" quote 
+        **$pakyu** -> <$pakyu @someone> to send someone middle fingers 
+        **$enlighten** -> receive the answer to life, the universe, and everything 
+        
+        **MUDAE INTEGRATED COMMANDS** 
+        **$claimcheck** -> check if someone have claims 
+        `)
+    }
+
+
     if(message[0] === "$bonk") {
         msg.reply("🔨 BONK " + msg.content.substring(6) + ", you're going to horny jail 🔨")
     }
@@ -49,10 +85,6 @@ function gotMessage(msg){
     if(message[0] === "$pakyu") {
         msg.reply("🖕 " + msg.content.substring(7) + " 🖕")
     }
-
-    if(message[0] === "$bonkbot") {
-        msg.reply("Tangina mo")
-    }
     if(message[0] === "$enlighten") {
         msg.reply("Mas marami kang free time pag wala kang org")
     }
@@ -62,18 +94,22 @@ function gotMessage(msg){
     if(message[0].substring(0,1) == "$" && message[0].substring(1,2) != "w" && message[0].substring(2,3) == "a" && message[0].length == 3){
         msg.reply("Muntikan na")
     }
+
     if(message[1] == "you" && message[3] == "claim"){
         claimTimer[message[0].substring(0,message[0].length - 1)] = message[2]
-        
-        
     }
+    
+    if(message[4] == "are" && message[5] == "now" && message[6] == "married!"){
+        claimTimer[message[1]] = "can't"
+    }
+        
     if(message[0] == "$claimcheck"){
-        // let timerOutput = "Claim status (**Mudae**) of the server\n"
-        // for (const [key, value] of Object.entries(claimTimer)) {
-        //     console.log(key, value);
-        //     timerOutput += key + ": " + value + "\n"
-        // }
-        // msg.reply(timerOutput)
-        msg.reply("Under maintenance")
+        let timerOutput = "Claim status (**Mudae**) of the server\n"
+        for (const [key, value] of Object.entries(claimTimer)) {
+            console.log(key, value);
+            timerOutput += key + ": " + value + "\n"
+        }
+        msg.reply(timerOutput)
+        //msg.reply("Under maintenance")
     }
 }
